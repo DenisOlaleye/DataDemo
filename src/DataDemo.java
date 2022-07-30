5c962ed5f036151cef0d7eb87360178f52ab87e2
DenisOlaleye <denis.olaleye@gmail.com>
2022-07-30 18:59:26 +0300

    DataDemo 2.0

--- src/DataDemo.java
+++ src/DataDemo.java
@@ -1,4 +1,17 @@
 
-public class DataDemo {
-
+public class DataDemo 
+{
+	public static void main (String [] args)
+	{
+		int aWholeNumber = 315;
+		final int STATES_IN_US = 50;
+		
+		
+		
+		System.out.print("The number is ");
+		System.out.println(aWholeNumber);
+		System.out.println("The number of states is " +STATES_IN_US);
+		
+	}
+	
 }
